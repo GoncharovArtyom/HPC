8a4ffa8b0c21f98fd876a6cfe630434c4e12f22d
Goncharov.Ar <Goncharov.Ar@digdes.com>
2018-05-10 15:07:14 +0300

    mistakes correction

--- generate_zeta_parallel.hh
+++ generate_zeta_parallel.hh
@@ -11,12 +11,88 @@ namespace parallel {
     using namespace std;
 
     struct ZetaGenerationBlock {
-        ZetaGenerationBlock(mutex& mtx_):mtx(mtx_){}
+        int t_start;
+        int x_start;
+        int y_start;
 
-        bool is_available() {
-            int t_id_prev = t_id - 1;
-            int x_id_prev = x_id - 1;
-            int y_id_prev = y_id - 1;
+        int t_end;
+        int x_end;
+        int y_end;
+
+        int x_id;
+        int y_id;
+        int t_id;
+    };
+
+    struct ZetaGenerationController {
+
+        ZetaGenerationController(int t_step_, int x_step_, int y_step_, int t_max, int x_max, int y_max):
+                x_step(x_step_), t_step(t_step_), y_step(y_step_) {
+            size_t t_size = ceil(double(t_max) / t_step);
+            size_t x_size = ceil(double(x_max) / x_step);
+            size_t y_size = ceil(double(y_max) / y_step);
+            completed.resize(t_size, x_size, y_size);
+
+            for (int current_t_start = 0; current_t_start < t_max; current_t_start += t_step) {
+                int current_t_end;
+                if ((current_t_start + t_step) <= t_max)
+                    current_t_end = current_t_start + t_step;
+                else
+                    current_t_end = t_max;
+
+                for (int current_x_start = 0; current_x_start < x_max; current_x_start += x_step) {
+                    int current_x_end;
+                    if ((current_x_start + x_step) <= x_max)
+                        current_x_end = current_x_start + x_step;
+                    else
+                        current_x_end = x_max;
+
+                    for (int current_y_start = 0; current_y_start < y_max; current_y_start += y_step) {
+                        int current_y_end;
+                        if ((current_y_start + y_step) <= y_max)
+                            current_y_end = current_y_start + y_step;
+                        else
+                            current_y_end = y_max;
+
+                        ZetaGenerationBlock current_block;
+                        current_block.t_start = current_t_start;
+                        current_block.t_end = current_t_end;
+                        current_block.x_start = current_x_start;
+                        current_block.x_end = current_x_end;
+                        current_block.y_start = current_y_start;
+                        current_block.y_end = current_y_end;
+                        current_block.t_id = current_t_start / t_step;
+                        current_block.x_id = current_x_start / x_step;
+                        current_block.y_id = current_y_start / y_step;
+
+                        queue.push_back(current_block);
+                    }
+                }
+            }
+        }
+
+        bool find_available(ZetaGenerationBlock &available_block) {
+            lock_guard <mutex> lock(mtx);
+
+            if (queue.size() == 0) {
+                return false;
+            }
+
+            for (auto current = queue.begin(); current != queue.end(); ++current) {
+                if (current->is_available()) {
+                    available_block = *current;
+                    queue.erase(current);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        bool is_available(ZetaGenerationBlock &block) {
+            int t_id_prev = block.t_id - 1;
+            int x_id_prev = block.x_id - 1;
+            int y_id_prev = block.y_id - 1;
 
             lock_guard <mutex> lock(mtx);
 
@@ -52,89 +128,9 @@ namespace parallel {
             return true;
         }
 
-        void set_completed() {
+        void set_completed(ZetaGenerationBlock &block) {
             lock_guard <mutex> lock(mtx);
-            completed(t_id, x_id, y_id) = true;
-        }
-
-        int t_start;
-        int x_start;
-        int y_start;
-
-        int t_end;
-        int x_end;
-        int y_end;
-
-        int x_id;
-        int y_id;
-        int t_id;
-
-        mutex& mtx;
-    };
-
-    struct ZetaGenerationController {
-
-        ZetaGenerationController(int t_step_, int x_step_, int y_step_, int t_max, int x_max, int y_max):
-                x_step(x_step_), t_step(t_step_), y_step(y_step_) {
-            size_t t_size = ceil(double(t_max) / t_step);
-            size_t x_size = ceil(double(x_max) / x_step);
-            size_t y_size = ceil(double(y_max) / y_step);
-            completed.resize(t_size, x_size, y_size);
-
-            for (int current_t_start = 0; current_t_start < t_max; current_t_start += t_step) {
-                int current_t_end;
-                if ((current_t_start + t_step) <= t_max)
-                    current_t_end = current_t_start + t_step;
-                else
-                    current_t_end = t_max;
-
-                for (int current_x_start = 0; current_x_start < x_max; current_x_start += x_step) {
-                    int current_x_end;
-                    if ((current_x_start + x_step) <= x_max)
-                        current_x_end = current_x_start + x_step;
-                    else
-                        current_x_end = x_max;
-
-                    for (int current_y_start = 0; current_y_start < y_max; current_y_start += y_step) {
-                        int current_y_end;
-                        if ((current_y_start + y_step) <= y_max)
-                            current_y_end = current_y_start + y_step;
-                        else
-                            current_y_end = y_max;
-
-                        ZetaGenerationBlock current_block(mtx);
-                        current_block.t_start = current_t_start;
-                        current_block.t_end = current_t_end;
-                        current_block.x_start = current_x_start;
-                        current_block.x_end = current_x_end;
-                        current_block.y_start = current_y_start;
-                        current_block.y_end = current_y_end;
-                        current_block.t_id = current_t_start / t_step;
-                        current_block.x_id = current_x_start / x_step;
-                        current_block.y_id = current_y_start / y_step;
-
-                        queue.push_back(current_block);
-                    }
-                }
-            }
-        }
-
-        bool find_available(ZetaGenerationBlock &available_block) {
-            lock_guard <mutex> lock(mtx);
-
-            if (queue.size() == 0) {
-                return false;
-            }
-
-            for (auto current = queue.begin(); current != queue.end(); ++current) {
-                if (current->is_available()) {
-                    available_block = *current;
-                    queue.erase(current);
-                    return true;
-                }
-            }
-
-            return false;
+            completed(block.t_id, block.x_id, block.y_id) = true;
         }
 
         blitz::Array<bool, 3> completed;
