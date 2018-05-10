2655571ee336a7875d0851af673ef4db771b57b4
Goncharov.Ar <Goncharov.Ar@digdes.com>
2018-05-10 16:45:14 +0300

    correct mistakes

--- autoreg.hh
+++ autoreg.hh
@@ -242,7 +242,8 @@ namespace autoreg {
 		std::vector<std::thread> threads;
 
 		for(int thread_id=0; thread_id< n_threads; ++thread_id){
-			std::thread current_thread(generate_zeta_parallel_worker<T>, std::ref(controller));
+			std::thread current_thread(generate_zeta_parallel_worker<T>, std::ref(phi), std::ref(zeta),
+									   std::ref(controller));
 			threads.push_back(std::move(current_thread));
 		}
 
