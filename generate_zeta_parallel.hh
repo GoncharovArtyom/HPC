e19ad713ce5195a9ccc26843ffff50087225dc2a
artem.goncharov <artem.goncharov@e.com>
2018-05-10 15:25:12 +0300

    ZetaGenerationController is done.

--- generate_zeta_parallel.hh
+++ generate_zeta_parallel.hh
@@ -79,7 +79,7 @@ namespace parallel {
             }
 
             for (auto current = queue.begin(); current != queue.end(); ++current) {
-                if (is_available(current)) {
+                if (is_available(*current)) {
                     available_block = *current;
                     queue.erase(current);
                     return true;
